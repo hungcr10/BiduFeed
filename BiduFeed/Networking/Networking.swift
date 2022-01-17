851866119921b32ce88941768c2a32db7a2a3f5d
daohung <cristianodaohung1999@gmail.com>
2022-01-17 16:27:17 +0700

    update

--- BiduFeed/Networking/Networking.swift
+++ BiduFeed/Networking/Networking.swift
@@ -43,18 +43,29 @@ class Networking {
             guard let data = data else { return }
             let datas = try! Data(contentsOf: data)
             completion(datas)
-            
-            do {
-                try FileManager.default.moveItem(at: data, to: documents.appendingPathComponent(getImageName(from: url)!))
-            } catch {
-                print(error)
-                
+            if FileManager.default.fileExists(atPath: documents.path) {
+                let imageData = loadImage(from: documents)
+            } else {
+                do {
+                    try FileManager.default.moveItem(at: data, to: documents.appendingPathComponent(imageDataName(from: url)!))
+                } catch {
+                    print(error)
+                    
+                }
             }
+            
             print("alo \(documents)")
         }
         task.resume()
     }
-     func getImageName(from urlString: String) -> String? {
+    func loadImage(from url: URL){
+      do {
+        let imageData = try Data(contentsOf: url)
+      } catch {
+        print(error.localizedDescription)
+     }
+    }
+     func imageDataName(from urlString: String) -> String? {
       guard var base64String = urlString.data(using: .utf8)?.base64EncodedString() else {return nil}
       base64String = base64String.components(separatedBy: CharacterSet.alphanumerics.inverted).joined()
       guard base64String.count < 50 else {
