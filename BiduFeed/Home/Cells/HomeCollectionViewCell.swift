29a5410394600057123bc9713f58add53d669d12
daohung <cristianodaohung1999@gmail.com>
2022-01-19 15:33:31 +0700

    k

--- BiduFeed/Home/Cells/HomeCollectionViewCell.swift
+++ BiduFeed/Home/Cells/HomeCollectionViewCell.swift
@@ -9,24 +9,10 @@ class HomeCollectionViewCell: UICollectionViewCell {
     }
     
     func configUI(urlImage: String) {
-        if FileManager.default.fileExists(atPath: Networking.shared.documents.path) {
-            let fileUrl = Networking.shared.documents.appendingPathComponent(urlImage)
-            do {
-                    let imageData = try? Data(contentsOf: fileUrl)
-                    guard let imageData = imageData else {return}
-                    self.mainImageView.image = UIImage(data: imageData)
-                
-            } catch {
-                print("error to load image\(error)")
-            }
-        } else {
             Networking.shared.fetchImage(url: urlImage) { data in
                 DispatchQueue.main.async {
                     self.mainImageView.image = UIImage(data: data)
                 }
-            }
         }
-        
     }
-    
 }
