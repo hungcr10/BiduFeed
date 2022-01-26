0e6aa2b668e364ac673b4b5d2a5fa185dae50e74
daohung <cristianodaohung1999@gmail.com>
2022-01-26 10:13:03 +0700

    updates

--- BiduFeed/Home/Cells/HomeCollectionViewCell.swift
+++ BiduFeed/Home/Cells/HomeCollectionViewCell.swift
@@ -18,13 +18,16 @@ extension HomeCollectionViewCell {
                                                        in: .userDomainMask).first!
         let myImgs = FileManager.default.urls(for: .documentDirectory,
                                                         in: .userDomainMask).first!.appendingPathComponent(Constants.URLImages)
-        
-            
+        if !FileManager.default.fileExists(atPath: myImgs.absoluteString) {
+          try? FileManager.default.createDirectory(atPath: myImgs.path, withIntermediateDirectories: false, attributes: nil)
+        }
         guard let urlImage = urlImage else { return }
         let myFile = urlImage.lastPathComponent
-        let imageFileDocument = myImgs.appendingPathComponent(myFile)
-        if FileManager.default.fileExists(atPath: imageFileDocument.path) {
-            let imageData = try! Data(contentsOf: imageFileDocument)
+        let imageFileDocument = fileDocument.appendingPathComponent(myFile).lastPathComponent
+        let fileImgs = myImgs.appendingPathComponent(imageFileDocument)
+        print("aa", fileImgs)
+        if FileManager.default.fileExists(atPath: fileImgs.path) {
+            let imageData = try! Data(contentsOf: fileImgs)
             mainImageView.image = UIImage(data: imageData)
         } else {
             Networking.shared.fetchImage(url: urlImage.absoluteString) { data in
@@ -32,9 +35,9 @@ extension HomeCollectionViewCell {
                     self.mainImageView.image = UIImage(data: data)
                 }
                 do {
-                    try data.write(to: myImgs)
+                    try data.write(to: fileImgs)
                     
-                    let directoryContents = try? FileManager.default.contentsOfDirectory(at: myImgs, includingPropertiesForKeys: nil, options: [])
+                    let directoryContents = try? FileManager.default.contentsOfDirectory(at: fileImgs, includingPropertiesForKeys: nil, options: [])
                     guard let directoryContents = directoryContents else {return}
                     if directoryContents.count > 50 {
                         try? FileManager.default.removeItem(at: directoryContents.first!)
